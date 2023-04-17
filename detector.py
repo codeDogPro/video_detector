e029d0fa7f43607fb9fd74dedc8eb4721ef16f62
code_doggy <1178297803@qq.com>
2023-04-17 14:35:27 +0800

    init repo, video detector V0.5

---
+++ detector.py
@@ -0,0 +1,60 @@
+import torch
+import numpy as np
+import cv2
+
+import mmcv
+from mmdet.apis import init_detector, inference_detector
+from mmdet.visualization import DetLocalVisualizer
+
+
+class Detector:
+    def __init__(self):
+        # 指定模型的配置文件和 checkpoint 文件路径
+        config_file = 'configs/rtmdet_tiny_8xb32-300e_coco.py'
+        checkpoint_file = 'checkpoints/rtmdet_tiny_8xb32-300e_coco_20220902_112414-78e30dcc.pth'
+        device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+
+        # init detector and visualizer
+        self.model = init_detector(config_file, checkpoint_file, device=device)
+        self.visualizer = DetLocalVisualizer()
+
+    def open_video(self, file_path):
+        assert file_path is not None
+        self.video = mmcv.VideoReader(file_path)
+        self.detec_video = True
+    
+    def open_camera(self, camera_id = 0):
+        self.camera = cv2.VideoCapture(camera_id)
+        self.detec_video = False
+
+    def read_video(self) -> np.ndarray:
+        frame = self.video.read()
+        if frame is None:
+            raise Exception('Fail to read video!')
+        return frame
+
+    def read_camera(self) -> np.ndarray:
+        _, frame = self.camera.read()
+        if frame is None:
+            raise Exception('Fail to read camera!')
+        return frame
+
+    def detect(self):
+        """ This function will read a frame and detect objects.
+            It will finally return a ndarray.
+        Returns:
+            _type_: numpy.ndarray
+        """
+        frame = self.read_video() if self.detec_video else self.read_camera()
+
+        results = inference_detector(self.model, frame)
+        self.visualizer.add_datasample(
+            name='video_detector',
+            image=frame,
+            data_sample=results,
+            draw_gt=False,
+            show=False
+        )
+        res_img = self.visualizer.get_image()
+        return res_img
+        
---
+++ main.py
@@ -0,0 +1,63 @@
+import sys
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
+from main_window import Ui_MainWindow
+
+from detector import Detector
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.setupUi(self)
+        # detector
+        self.detector = Detector()
+        # qt gui
+        self.timer = QtCore.QTimer(self) # 定时器，用于播放视频
+        self.timer.timeout.connect(self.refreshFrame)
+        self.import_btn.clicked.connect(self.openVideo)
+        self.start_btn.clicked.connect(self.playVideo)
+        self.stop_btn.clicked.connect(self.pauseVideo)
+
+    def openVideo(self):
+        file_path, _ = QFileDialog.getOpenFileName(self, '选择文件', './', 
+                                                   '视频文件(*.mp4 *.mkv *.mov *.avi *.flv)')
+        self.file_path.setText(file_path)  # self.file_path inherit Ui_MainWindow.fila_path
+        self.detector.open_video(file_path)
+    
+    def openCamera(self):
+        # TODO: add a button to open camera
+        pass
+
+    def playVideo(self):
+        self.timer.start(50)
+        self.start_btn.setEnabled(False)
+        self.stop_btn.setEnabled(True)
+
+    def pauseVideo(self):
+        self.timer.stop()
+        self.start_btn.setEnabled(True)
+        self.stop_btn.setEnabled(False)
+
+    def refreshFrame(self):
+        res_img = self.detector.detect()
+        q_image = QImage(res_img,
+                         res_img.shape[1],
+                         res_img.shape[0],
+                         QImage.Format_RGB888
+                        ).rgbSwapped().scaled(self.label.width(),
+                                              self.label.height())
+        self.label.setPixmap(QPixmap.fromImage(q_image))
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+    
+
+if __name__ == '__main__':
+    main()
+    
